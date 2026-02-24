8b69eed3d2d45aa312531b7cc6b79ec8e8da3141
Elise Verhoeye <eliseverhoeye@hotmail.com>
2026-02-24 10:25:00 +0100

    Feat: Wrapped logo with Link to /

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import { headerQuery } from "@/lib/sanity/queries";
 import { client } from "@/lib/sanity/client";
 import SanityImage from "./SanityImage";
 import { SanityDocument } from "next-sanity";
+import Link from "next/link";
 
 type HeaderProps = {
   variant?: "overlay" | "default";
@@ -25,16 +26,24 @@ const Header = async ({ variant = "default" }: HeaderProps) => {
         left: 0,
         width: "100%",
         zIndex: 1000,
-        padding: "20px", 
+        padding: "20px",
         display: "flex",
         alignItems: "center",
         justifyContent: "space-between",
         background: isOverlay ? "rgba(255,255,255,0.1)" : "white",
         backdropFilter: isOverlay ? "blur(10px)" : "none",
-        borderBottom: isOverlay ? "none" : "1px solid #ddd"      }}
+        borderBottom: isOverlay ? "none" : "1px solid #ddd",
+      }}
     >
       {header.logo && (
-        <SanityImage value={header.logo} alt="Logo" width={120} height={60} />
+        <Link href="/">
+            <SanityImage
+              value={header.logo}
+              alt="Logo"
+              width={120}
+              height={60}
+            />
+        </Link>
       )}
 
       <nav style={{ display: "flex", gap: 20 }}>
